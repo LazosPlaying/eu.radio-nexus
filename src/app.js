332fb30d9721a2ae67041eb1b1a35f340da1a5a7
aljaxus <aljaxus.dev@gmail.com>
2018-08-21 22:33:12 +0200

    updated

--- src/app.js
+++ src/app.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
 	// SET PRELOADER OPACITY TO 1 (100%)
 	$('body').css('opacity', '1');
+
+	// COOKIES INITIALISATION
+	{
+		(Cookies.get('volume') == undefined) ? Cookies.set('volume', 5) : null;
+		(Cookies.get('volumeMuted') == undefined) ? Cookies.set('volumeMuted', false) : null;
+	}
+});
+
+//
+// INITIALISE FUNCTIONS
+//
+
+function toggleMuteVolume(){
+	$btn = $('#volume-toggle').children('button');
+	$state = Cookies.get('volumeMuted');
+
+	if ($state === true){																												// VOLUME IS MUTED - WE WILL UNMUTE IT
+		Cookies.set('volumeMuted', false);
+		$btn.removeClass('mdl-button--colored');
+
+	} else if ($state === false){																										// VOLUME IS UNMUTED - WE WILL MUTE IT
+		Cookies.set('volumeMuted', true);
+		$btn.addClass('mdl-button--colored');
+
+	} else {
+		if(!alert('Something went wrong! Please, reload the page')){window.location.reload();}
+	}
+
+}
+
+//
+// INITIALISE TRIGGERS
+//
+
+$('#volume-toggle').children('button').on('click', function(){
+	toggleMuteVolume();
 });
