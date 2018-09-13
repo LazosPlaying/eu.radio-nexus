0f9b2686930f4b9af9dfb83a839a1717d53172fe
aljaxus <aljaxus.dev@gmail.com>
2018-09-13 23:30:04 +0200

    Added "edited by" field
    Added support for displaying a custom field ("edited by") for specifying the author of a remix, if the track is a mix or an edit from the original one

--- src/app.js
+++ src/app.js
@@ -115,6 +115,7 @@ function nowPlayingUpdate(){
 		$('#nowplaying').find('#nowplaying-thumbnail-box').html($img);
 		$('#nowplaying').find('#nowplaying-title').html($data.song.title);
 		$('#nowplaying').find('#nowplaying-artist').html($data.song.artist);
+		$('#nowplaying').find('#nowplaying-editby').html($data.song.custom_fields.edit_by);
 
 		{
 			let timenow = $data.elapsed;
